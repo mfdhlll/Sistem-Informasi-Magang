b795bf2931e9b2817bdd5e384bca7e87ff913520
Achmad Yuan Fadhillah <fadhil.yuann@gmail.com>
2026-07-14 15:35:11 +0700

    buat view/components/hero

---
+++ views/components/hero.php
@@ -0,0 +1,24 @@
+<section class="hero">
+
+    <div class="overlay"></div>
+
+    <div class="container hero-content text-center">
+
+        <h1>Sistem Informasi Magang Mahasiswa</h1>
+
+        <p>
+            Temukan informasi perusahaan dan lowongan magang
+            yang sesuai dengan minat dan kemampuanmu.
+        </p>
+
+        <a href="#search" class="btn btn-light btn-lg">
+
+            <i class="bi bi-search"></i>
+
+            Cari Tempat Magang
+
+        </a>
+
+    </div>
+
+</section>
