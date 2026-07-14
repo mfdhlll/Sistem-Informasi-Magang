bb17da6820a930d558e060758f52136c21737814
Achmad Yuan Fadhillah <fadhil.yuann@gmail.com>
2026-07-14 15:35:59 +0700

    buat views/components/search

---
+++ views/components/search.php
@@ -0,0 +1,23 @@
+<div class="container mt-5">
+
+    <form action="index.php" method="GET">
+
+        <input type="hidden" name="page" value="search">
+
+        <div class="input-group">
+
+            <input type="text" name="keyword" class="form-control" placeholder="Cari perusahaan, bidang atau lokasi...">
+
+            <button class="btn btn-primary">
+
+                <i class="bi bi-search"></i>
+
+                Cari
+
+            </button>
+
+        </div>
+
+    </form>
+
+</div>
